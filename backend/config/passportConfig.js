6daa04ec8dbd86e530e8140bee55b30dbb0ea0f4
Ashray Cp <ashraycp07@gmail.com>
2025-03-16 12:39:30 +0530

    updated callbackURl in passport

--- backend/config/passportConfig.js
+++ backend/config/passportConfig.js
@@ -7,7 +7,7 @@ const GoogleStrategy = require("passport-google-oauth20").Strategy;
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://sangeetbox.onrender.com/login/google/callback",
+    callbackURL: "http://sangeetbox.onrender.com/auth/login/google/callback",
     scope: ["profile", "email"]
 }, async (accessToken, refreshToken, profile, done) => {
     
